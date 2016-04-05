40e88d2604ccbd6396ece5498580be0f22b8cea5
Hari Krishna <bkrishna-hari@users.noreply.github.com>
2016-04-05 12:00:59 +0530

    Added AzureRm & ResourceName

--- Check-StorSimpleVolume-AssetsInput.ps1
+++ Check-StorSimpleVolume-AssetsInput.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 .DESCRIPTION
     This runbook creates all assets which required for Disk Integrity process.
      
@@ -21,11 +21,11 @@
     MailPort (Optional): Port number of SmtpServer. Not mandatory when the port no is 25 (Default)
     MailTo: To email address to send final CHKDSK result
     MailCc (Optional): Cc email address to send final CHKDSK result.
-	AutomationAccountName: The name of the Aumation account name.
-
+    AutomationAccountName: The name of the Aumation account name.
+    
 .NOTES:
     Multi Factor Authentication must be disabled to execute this runbook
-	Multiple email addresses cannot support in To / Cc address field
+    Multiple email addresses cannot support in To / Cc address field
 #>
 
 workflow Check-StorSimpleVolume-AssetsInput
@@ -33,66 +33,68 @@ workflow Check-StorSimpleVolume-AssetsInput
     Param
     (
         [parameter(Mandatory=$true, Position=1, HelpMessage="The name of the Azure Subscription")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$AzureSubscriptionName,
         
         [parameter(Mandatory=$true, Position=2, HelpMessage="The name of the StorSimple resource")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$ResourceName,
         
         [parameter(Mandatory=$true, Position=3, HelpMessage="The registration key for the StorSimple manager")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$StorSimRegKey,
         
         [parameter(Mandatory=$true, Position=4, HelpMessage="The storage account name in which the script will be stored")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$StorageAccountName,
         
         [parameter(Mandatory=$true, Position=5, HelpMessage="The access key for the storage account")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$StorageAccountKey,
         
         [parameter(Mandatory=$true, Position=6, HelpMessage="The Device which has to be verified disk status")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$SourceDeviceName,
         
         [parameter(Mandatory=$true, Position=7, HelpMessage="The Device on which the containers are to be cloned")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$TargetDeviceName,
         
         [parameter(Mandatory=$true, Position=8, HelpMessage="The name of the Virtual machine which has to be used for mount the volumes & verify the disk status.")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$VMName,
         
         [parameter(Mandatory=$true, Position=9, HelpMessage="The Cloud service name where Virtual machine is running")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$VMServiceName,
         
         [parameter(Mandatory=$true, Position=10, HelpMessage="A comma separated string of volume containers present on the Device that need to be checked, ex - VolCon1,VolCon2")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$VolumeContainers,
         
         [parameter(Mandatory=$true, Position=11, HelpMessage="The name of the SmtpServer")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$MailSmtpServer,
         
         [parameter(Mandatory=$false, Position=12, HelpMessage="Port number (Optional) of SmtpServer. Not mandatory when the port no is 25 (Default)")]
-		[ValidateRange(0,10)]
+        [ValidateRange(0,10)]
         [Int]$MailSmtpPortNo=25,
         
         [parameter(Mandatory=$true, Position=13, HelpMessage="To email address to send final CHKDSK result")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [string]$MailTo,
         
         [parameter(Mandatory=$false, Position=14, HelpMessage="Cc email address (Optional) to send final CHKDSK result.")]
         [string]$MailCc,
 
         [parameter(Mandatory=$true, Position=15, HelpMessage="The name of the Aumation account name")]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [String]$AutomationAccountName
     )
     
-	# Add all new assets in collection object
+    # Add all new assets in collection object
+    Write-Output "Adding assets into collection object"
+    
     $NewAssetList = @()
     $AssetProp = @{ Name="AzureSubscriptionName"; Value=$AzureSubscriptionName; IsMandatory=$true; }
     $AssetObj = New-Object PSObject -Property $AssetProp
@@ -150,23 +152,24 @@ workflow Check-StorSimpleVolume-AssetsInput
     $AssetObj = New-Object PSObject -Property $AssetProp
     $NewAssetList += $AssetObj
 	
-	# Validate all mandatory parameters
-	InlineScript 
-	{
- 		$NewAssetList = $Using:NewAssetList
-		$ErrorMessage = [string]::Empty
+    # Validate all mandatory parameters
+    Write-Output "Validating all mandatory assets"
+    InlineScript 
+    {
+        $NewAssetList = $Using:NewAssetList
+        $ErrorMessage = [string]::Empty
         
-		foreach ($NewAssetData in $NewAssetList) {
-			If ($NewAssetData.IsMandatory -and [string]::IsNullOrEmpty($NewAssetData.Value)) { 
-	            $ErrorMessage += "$($NewAssetData.Name) cannot be blank. `n" 
-	        }
-		}
-		
+        foreach ($NewAssetData in $NewAssetList) {
+            If ($NewAssetData.IsMandatory -and [string]::IsNullOrEmpty($NewAssetData.Value)) { 
+                $ErrorMessage += "$($NewAssetData.Name) cannot be blank. `n" 
+            }
+        }
+        
         # Display message
-		If ([string]::IsNullOrEmpty($ErrorMessage) -eq $false) {
-			throw $ErrorMessage
-		}
-	}
+        If ([string]::IsNullOrEmpty($ErrorMessage) -eq $false) {
+            throw $ErrorMessage
+        }
+    }
     
     # Fetch basic Azure automation variables
     $AzureCredential = Get-AutomationPSCredential -Name "AzureCredential"
@@ -177,37 +180,46 @@ workflow Check-StorSimpleVolume-AssetsInput
     
     # Connect to Azure
     Write-Output "Connecting to Azure"
-    $AzureAccount = Add-AzureAccount -Credential $AzureCredential      
-    $AzureSubscription = Select-AzureSubscription -SubscriptionName $AzureSubscriptionName          
+    $AzureAccount = Add-AzureRmAccount -Credential $AzureCredential      
+    $AzureSubscription = Select-AzureRmSubscription -SubscriptionName $AzureSubscriptionName  
     If (($AzureSubscription -eq $null) -or ($AzureAccount -eq $null))
     {
         throw "Unable to connect to Azure"
     }
     
-    # Fetch asset list in Automation account
+    # Read automation account resource group
+    Write-Output "Reading automation account's resource group name"
     try {
-        $AssetList = (Get-AzureAutomationVariable -AutomationAccountName $AutomationAccountName)
+        $ResourceGroupName = (Get-AzureRmAutomationAccount | where AutomationAccountName -eq $AutomationAccountName).ResourceGroupName
+    }
+    catch {
+        throw "Failed to read automation account's resource group"
+    }
+    # Fetch asset list in Automation account
+    Write-Output "Fetching all existing assets info"
+    try {
+        $AssetList = (Get-AzureRmAutomationVariable -AutomationAccountName $AutomationAccountName -ResourceGroupName $ResourceGroupName)
     }
     catch {
         throw "The Automation account ($AutomationAccountName) is not found."
     }
 
     
-	Write-Output "Initiating to create assets"
+    Write-Output "Attempting to create/update asset(s)"
     foreach ($NewAssetData in $NewAssetList)
     {
         $AssetVariableName = $NewAssetData.Name
         $AssetValue = $NewAssetData.Value
 		
-		# Print asset name & value
-		Write-Output "$AssetVariableName - $AssetValue"
+        # Print asset name & value
+        Write-Output "$AssetVariableName : $AssetValue"
         
-        If ((($AssetList) | Where-Object {$_.Name -eq $AssetVariableName}) -ne $null) {
-            $asset = Set-AzureAutomationVariable -AutomationAccountName $AutomationAccountName -Name $AssetVariableName -Encrypted $false -Value $AssetValue
+        If ($AssetList -ne $null -and (($AssetList) | Where-Object {$_.Name -eq $AssetVariableName}) -ne $null) {
+            $asset = Set-AzureRmAutomationVariable -AutomationAccountName $AutomationAccountName -Name $AssetVariableName -ResourceGroupName $ResourceGroupName -Value $AssetValue -Encrypted $false
             Write-Output "$AssetVariableName asset updated"
         }
-        else {
-            $asset = New-AzureAutomationVariable -AutomationAccountName $AutomationAccountName -Name $AssetVariableName -Value $AssetValue -Encrypted $false
+        elseIf ($AssetList -ne $null) {
+            $asset = New-AzureRmAutomationVariable -AutomationAccountName $AutomationAccountName -Name $AssetVariableName -Value $AssetValue -Encrypted $false -ResourceGroupName $ResourceGroupName
             Write-Output "$AssetVariableName asset created"
         }
     }
